e105b89fdd5b272bd2b840dbcf19bc41451652d8
shalant <doug.rosenberg@gmail.com>
2024-06-26 14:31:15 -0500

    working on login/logout, bug with refresh

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { faBars, faChartLine, faFlagUsa, faGear, faGlobe, faUser, faWheatAwn } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { environment } from '../environments/environment.development';
+import { AuthService } from './features/auth/services/auth.service';
+import { User } from './features/auth/models/user.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +14,7 @@ import { environment } from '../environments/environment.development';
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
+    CommonModule,
     // NavbarComponent,
     //     HeaderComponent,
     //     DarkToggleComponent,
@@ -20,8 +24,11 @@ import { environment } from '../environments/environment.development';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'Frontend';
+
+  user?: User;
 
   faWheatAwn = faWheatAwn;
   faChartLine = faChartLine;
@@ -31,11 +38,30 @@ export class AppComponent {
   faUser = faUser;
   faBars = faBars;
 
+  constructor(private authService: AuthService,
+    private router: Router
+  ) {}
+
   ngOnInit() {
     if (environment.production) {
       console.log("We are running in production mode");
     } else {
       console.log("We are running in development mode");
     }
+
+    this.authService.user()
+      .subscribe({
+        next: (response) => {
+          this.user = response;
+        }
+      })
+    
+    this.user = this.authService.getUser();
+
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigateByUrl('/');
   }
 }
--- Frontend/src/app/features/auth/login/login.component.ts
+++ Frontend/src/app/features/auth/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { LoginRequest } from '../models/login-request.model';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -16,7 +18,10 @@ export class LoginComponent {
 
   model: LoginRequest;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+    private cookieService: CookieService,
+    private router: Router
+  ) {
     this.model = {
       email: '',
       password: ''
@@ -27,7 +32,19 @@ export class LoginComponent {
     this.authService.login(this.model)
       .subscribe({
         next: (response) => {
-          console.log(response)
+          // Set Auth Cookie
+          this.cookieService.set('Authorization', `Bearer ${response.token}`,
+            undefined, '/', undefined, true, 'Strict');
+
+          // Set user in local storage, 
+          this.authService.setUser({
+            email: response.email,
+            roles: response.roles
+          })
+
+          // Redirect back to homepage
+          this.router.navigateByUrl('/')
+
         }
       })
   }
